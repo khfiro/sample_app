4baa56c314ecf3b88301e4724e7dc5fa6b479309
kaynaz Firoozabadi <khfiro@gmail.com>
2014-03-02 05:10:21 -0600

    Message

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 SampleApp::Application.routes.draw do
   get "users/new"
 
- root  'static_pages#home'
+root  'static_pages#Home'
 match '/signup',  to: 'users#new',               via: 'get'
-match '/help',    to: 'static_pages#help',       via: 'get'
+match '/help',    to: 'static_pages#Help',       via: 'get'
 match '/about',   to: 'static_pages#about',      via: 'get'
 match '/contact', to: 'static_pages#contact',    via: 'get'
 
