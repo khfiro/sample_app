f95d84baa100f9c6a83c16d64aac1a71b9145db5
kaynaz Firoozabadi <khfiro@gmail.com>
2014-03-04 02:16:30 -0600

    Changed route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 SampleApp::Application.routes.draw do
   get "users/new"
  root :to => 'static_pages#home'
- 
+ map.connect'',: controller => "index",: action => "index"
+
 match '/signup',  to: 'users#new',               via: 'get'
 match '/help',    to: 'static_pages#help',       via: 'get'
 match '/about',   to: 'static_pages#about',      via: 'get'
